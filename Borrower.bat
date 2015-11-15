036a38ed0bbd4e8e9b18dcd9b07017ed108bffe0
saquil <saquil_20@hotmail.com>
2015-11-15 20:32:09 +0100

    JMS part 1 application

---
+++ Borrower.bat
@@ -0,0 +1,4 @@
+
+
+java -cp -agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:49788 -Dfile.encoding=Cp1252 -classpath "C:\Users\Saquil Mohamed\workspace\JMS\bin;C:\Users\Saquil Mohamed\Desktop\JMS\javax.jms-1.1.jar;C:\Users\Saquil Mohamed\Desktop\JMS\apache-activemq-5.2.0\activemq-all-5.2.0.jar" ch04.QBorrower QueueCF LoanRequestQ LoanResponseQ
+pause
