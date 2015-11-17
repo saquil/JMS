d036b514d5bf4b9b6b046bbc3511da0dea52f7a3
saquil <saquil_20@hotmail.com>
2015-11-17 21:29:01 +0100

    Revert "JMS part 1 application"
    This reverts commit 036a38ed0bbd4e8e9b18dcd9b07017ed108bffe0.

--- TBorrower.bat
+++
@@ -1,4 +0,0 @@
-
-
-java -cp -agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:49788 -Dfile.encoding=Cp1252 -classpath "C:\Users\Saquil Mohamed\workspace\JMS\bin;C:\Users\Saquil Mohamed\Desktop\JMS\javax.jms-1.1.jar;C:\Users\Saquil Mohamed\Desktop\JMS\apache-activemq-5.2.0\activemq-all-5.2.0.jar" ch05.TBorrower TopicCF RateTopic 5.6
-pause
